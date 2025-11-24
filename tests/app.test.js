8e1b68db438f3a5cd82af2cd2973e6afe8b2d2d2
gguip1 <gguip7554@naver.com>
2025-11-24 14:32:33 +0900

    add: ci settings

---
+++ tests/app.test.js
@@ -0,0 +1,15 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+descript('GET /', () => {
+  it('should respond with Hello World!', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toEqual(200);
+    expect(response.text).toEqual('Hello World!');
+  });
+});
